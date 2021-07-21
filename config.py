ed0edcc983ec0a0b5a095f218a07461d15821a7e
Litu Zou <litu.zou@outlook.com>
2021-07-21 10:58:49 +0800

    changed epoch size

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch
 DATA_FILE = 'post_data.csv'
 BATCH_SIZE = 8
 NUM_WORKERS = 2
-EPOCH = 100
+EPOCH = 20
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 MAX_VOCAB_SIZE = 2000
 UNK = '<UNK>'
